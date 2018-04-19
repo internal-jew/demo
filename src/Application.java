a4c9782c46c03c831fad214dbc21f8004ca48066
Vladyslav Lubenskyi <vladyslav.lubenskyi@teamdev.com>
2018-04-19 21:13:28 +0300

    Try to create a conflict

--- src/Application.java
+++ src/Application.java
@@ -3,7 +3,7 @@ public class Application {
         System.out.println(1);
         System.out.println(2);
         System.out.println(3);
-        System.out.println(4);
+        System.out.println(8);
         System.out.println(5);
         System.out.println(5);
         System.out.println(8);
